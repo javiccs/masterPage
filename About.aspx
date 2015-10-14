b7e56c86d33d94ebf171f578dc942e2ebd4a7cc2
adosale <aucab21@gmail.com>
2015-10-14 09:08:28 -0430

    Implementación menú vertical dinámico
    Al correr el Inicio esta nos ubica en la página ppal. Si se corre el
    About.aspx el menú vertical cambia en base a las funcionalidades que
    este posee. Falta todavía link entre las ventanas y el idModulo en cada
    aspx.

--- About.aspx
+++ About.aspx
@@ -1,7 +1,7 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SKD.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="templateApp.About" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-<title>About</title>
+<title>PRUEBA MENU VERTICAL</title>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    About
+    PRUEBA OPCION 4 VISUAL
 </asp:Content>
--- SKD.Master.cs
+++ SKD.Master.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml;
+using System.Text;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,8 +11,32 @@ namespace templateApp
 {
     public partial class SKD : System.Web.UI.MasterPage
     {
+        private string idModulo;
+        private Dictionary<string, string> opcionesDelMenu = new Dictionary<string, string>();
+
+        public string IdModulo
+        {
+            get { return idModulo; }
+            set { idModulo = value; }
+        }
+        public Dictionary<string, string> OpcionesDelMenu
+        {
+            get { return opcionesDelMenu; }
+            set { opcionesDelMenu = value; }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Server.MapPath("menuLateral.xml"));
+            idModulo = IdModulo;
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+                foreach (XmlNode subNode in node.ChildNodes)
+                    if (!(subNode.Attributes["id"] == null) && subNode.Attributes["id"].InnerText.Equals(IdModulo))
+                    {
+                        OpcionesDelMenu[node.Attributes["nombre"].InnerText] = node.Attributes["link"].InnerText;
+                        break;
+                    }
 
         }
     }
